fafed7ec31dbeed9878db2cbe2b7be93de212809
Darkly Dreaming Wesker <weskerhluffy@gmail.com>
2017-08-11 07:06:25 -0500

    implementado filtrado

--- src/ass.c
+++ src/ass.c
@@ -31,7 +31,8 @@ typedef struct puto_polar {
 #define ASS_MAX_PUTOS 100000
 
 puto_cardinal coordenadas_cartesianas[ASS_MAX_PUTOS] = { 0 };
-puto_polar coordenadas_polares[ASS_MAX_PUTOS] = { 0 };
+puto_polar coordenadas_polares[ASS_MAX_PUTOS + 1] = { 0 };
+puto_cardinal *coordenadas_cartesianas_filtradas[ASS_MAX_PUTOS] = { 0 };
 
 puto_cardinal *MIN_COORD_CARD = &(puto_cardinal ) { .coord_xy = LONG_MAX };
 puto_polar *MAX_COORD_POLAR = &(puto_polar ) { .distancia_puto_polar = 0 };
@@ -119,6 +120,24 @@ int ass_comparar_puto_polar_inv_qsort(const void *a, const void *b) {
 	return ass_comparar_puto_polar_inv((puto_polar*) a, (puto_polar*) b);
 }
 
+static char ass_puto_dentro(puto_cardinal *puto, puto_cardinal *segmento_inicio,
+		puto_cardinal *segmento_fin) {
+	char result = 0;
+	double angulo = 0;
+	double angulo_puto = 0;
+
+	angulo = atan2(segmento_fin->coord_y - segmento_inicio->coord_y,
+			segmento_fin->coord_x - segmento_inicio->coord_x);
+
+	angulo_puto = atan2(puto->coord_y - segmento_inicio->coord_y,
+			puto->coord_x - segmento_inicio->coord_x);
+
+//	printf("el anogulo %f y el angulo puto %f\n", angulo, angulo_puto);
+	result = angulo_puto < angulo;
+
+	return result;
+}
+
 static void ass_main() {
 	natural num_casos = 0;
 	natural i, k;
@@ -130,6 +149,8 @@ static void ass_main() {
 		natural cont_coor = 0;
 		puto_cardinal *min_card = MIN_COORD_CARD;
 		puto_polar max_pol = *MAX_COORD_POLAR;
+		puto_polar *puto_polar_tmp = NULL;
+		natural coord_filtradas_tam = 0;
 		scanf("%u\n", &num_coor);
 
 		for (i = 0; i < num_coor; i++) {
@@ -143,11 +164,11 @@ static void ass_main() {
 				puto_cardinal *cur_puto = coordenadas_cartesianas + cont_coor++;
 				cur_puto->coord_x = coord_x_in;
 				cur_puto->coord_y = coord_y_in;
-			/*
-				printf("x %d(%x) y %d(%x) x-y %lld (%llx)\n", cur_puto->coord_x,
-						cur_puto->coord_x, cur_puto->coord_y, cur_puto->coord_y,
-						cur_puto->coord_xy, cur_puto->coord_xy);
-						*/
+				/*
+				 printf("x %d(%x) y %d(%x) x-y %lld (%llx)\n", cur_puto->coord_x,
+				 cur_puto->coord_x, cur_puto->coord_y, cur_puto->coord_y,
+				 cur_puto->coord_xy, cur_puto->coord_xy);
+				 */
 				if ((min_card->coord_x > cur_puto->coord_x)
 						|| (min_card->coord_x == cur_puto->coord_x
 								&& min_card->coord_y > cur_puto->coord_y)) {
@@ -162,9 +183,9 @@ static void ass_main() {
 
 			ass_puto_cartesiano_a_polar(min_card, cur_puto, cur_puto_pol);
 			/*
-			printf("ano %f dist %f\n", cur_puto_pol->anogulo_puto_polar,
-					cur_puto_pol->distancia_puto_polar);
-					*/
+			 printf("ano %f dist %f\n", cur_puto_pol->anogulo_puto_polar,
+			 cur_puto_pol->distancia_puto_polar);
+			 */
 			if (ass_comparar_puto_polar_dist(&max_pol, cur_puto_pol) <= 0) {
 				max_pol = *cur_puto_pol;
 			}
@@ -191,6 +212,47 @@ static void ass_main() {
 		if (idx_max_pol < cont_coor) {
 			qsort(coordenadas_polares + idx_max_pol, cont_coor - idx_max_pol,
 					sizeof(puto_polar), ass_comparar_puto_polar_inv_qsort);
+		}
+
+		puto_polar_tmp = coordenadas_polares + cont_coor;
+		puto_polar_tmp->cardinal_puto_polar = min_card;
+
+		coordenadas_cartesianas_filtradas[0] = min_card;
+		for (i = 1; i < 3; i++) {
+			coordenadas_cartesianas_filtradas[i] =
+					(coordenadas_polares + i)->cardinal_puto_polar;
+		}
+
+		coord_filtradas_tam = 3;
+
+		i = 3;
+		for (i = 0; i <= cont_coor; i++) {
+			puto_cardinal *segmento_fin = NULL;
+
+			segmento_fin = (coordenadas_polares + i)->cardinal_puto_polar;
+			while (coord_filtradas_tam > 2) {
+				puto_cardinal *segmento_ini = NULL;
+				puto_cardinal *puto = NULL;
+				char puto_dentro = 0;
+				segmento_ini =
+						coordenadas_cartesianas_filtradas[coord_filtradas_tam
+								- 2];
+				puto =
+						coordenadas_cartesianas_filtradas[coord_filtradas_tam
+								- 1];
+
+				puto_dentro = ass_puto_dentro(puto, segmento_ini, segmento_fin);
+
+				if (!puto_dentro) {
+					break;
+				}
+
+				coord_filtradas_tam--;
+			}
+
+			coordenadas_cartesianas_filtradas[coord_filtradas_tam] =
+					segmento_fin;
+
 		}
 
 		printf("%u\n", cont_coor);
